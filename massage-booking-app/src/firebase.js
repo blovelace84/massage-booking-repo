e39c58fa821c4d94d2abb5c748988b95f79c4a67
Brittani Lovelace <blovelace84@gmail.com>
2025-07-03 20:56:53 -0400

    Getting started on everything

---
+++ massage-booking-app/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDrmWvdXseVsuHUHtPuyKOb_OffI1abWbA",
+  authDomain: "massage-booking-app-d466b.firebaseapp.com",
+  projectId: "massage-booking-app-d466b",
+  storageBucket: "massage-booking-app-d466b.firebasestorage.app",
+  messagingSenderId: "310635094005",
+  appId: "1:310635094005:web:d697befe13e7433f3b4abc",
+  measurementId: "G-WR6CZMHT7W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
