58cf115f27517c3130257b8a0efdb65f4719facd
Brittani Lovelace <blovelace84@gmail.com>
2025-07-06 20:12:46 -0400

    redoing firebase

--- massage-booking-app/src/firebase.js
+++ massage-booking-app/src/firebase.js
@@ -1,20 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDrmWvdXseVsuHUHtPuyKOb_OffI1abWbA",
-  authDomain: "massage-booking-app-d466b.firebaseapp.com",
-  projectId: "massage-booking-app-d466b",
-  storageBucket: "massage-booking-app-d466b.firebasestorage.app",
-  messagingSenderId: "310635094005",
-  appId: "1:310635094005:web:d697befe13e7433f3b4abc",
-  measurementId: "G-WR6CZMHT7W"
+  apiKey: "AIzaSyAfnPjmkpYVhPYyLjGUEBqtZ-pTCHiw_2U",
+  authDomain: "massage-booking-project.firebaseapp.com",
+  projectId: "massage-booking-project",
+  storageBucket: "massage-booking-project.firebasestorage.app",
+  messagingSenderId: "764268554287",
+  appId: "1:764268554287:web:2081d0aba02e2afb8bbeda",
+  measurementId: "G-KYQWBD1MW9"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app); // Initial db from firebase/firestore
+const analytics = getAnalytics(app);
