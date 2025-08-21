db6b87bdb2afa57b35be56e823c1e2088028e909
Brittani Lovelace <blovelace84@gmail.com>
2025-08-21 07:24:55 -0400

    changed Navbar into a card nav design

--- massage-booking-app-project/src/components/Navbar.jsx
+++ massage-booking-app-project/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase/config";
+import "./Navbar.css"; // we'll add custom styles here
 
 const Navbar = () => {
   const { user } = useAuth();
@@ -16,45 +17,41 @@ const Navbar = () => {
   };
 
   return (
-    <div className="container mt-3">
-      <Nav variant="pills" className="justify-content-center bg-dark p-2 rounded">
+    <div className="container mt-4">
+      <Nav className="card-nav">
         <Nav.Item>
-          <Nav.Link as={NavLink} to="/" end>
+          <Nav.Link as={NavLink} to="/" end className="card-nav-item">
             Book
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link as={NavLink} to="/therapists">
-            Therapists
           </Nav.Link>
         </Nav.Item>
         {user && (
           <Nav.Item>
-            <Nav.Link as={NavLink} to="/my-bookings">
+            <Nav.Link as={NavLink} to="/my-bookings" className="card-nav-item">
               My Bookings
             </Nav.Link>
           </Nav.Item>
         )}
-
+        <Nav.Item>
+          <Nav.Link as={NavLink} to="/therapists" className="card-nav-item">
+            Therapists
+          </Nav.Link>
+        </Nav.Item>
         {!user ? (
           <>
             <Nav.Item>
-              <Nav.Link as={NavLink} to="/login">
+              <Nav.Link as={NavLink} to="/login" className="card-nav-item">
                 Login
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link as={NavLink} to="/register">
+              <Nav.Link as={NavLink} to="/register" className="card-nav-item">
                 Register
               </Nav.Link>
             </Nav.Item>
           </>
         ) : (
           <Nav.Item>
-            <button
-              className="btn btn-outline-danger ms-2"
-              onClick={handleLogout}
-            >
+            <button className="btn btn-outline-danger card-nav-item" onClick={handleLogout}>
               Logout
             </button>
           </Nav.Item>
